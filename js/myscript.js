9a264d1a0b3037be6ce9993994605e4c023f3d74
Al-Giovinetti <giovinettialessio@gmail.com>
2023-05-12 15:24:02 +0200

    Stampa a schermo delle variabili inserite

--- js/myscript.js
+++ js/myscript.js
@@ -3,6 +3,12 @@ const kmTravelled = parseInt (prompt("Quanti km devi percorrere ?"));
 
 const userAge = parseInt (prompt("Quanti anni hai ?"));
 
+//Stampa dei dati inseriti sulla pagina
+document.getElementById("km-travelled").innerHTML += kmTravelled;
+
+document.getElementById("user-age").innerHTML += userAge;
+
 //Inizzializzo una variabile per identificare il prezzo non scontato//
 
-let priceWithoutDiscount = kmTravelled * 0.21 
+let priceWithoutDiscount = kmTravelled * 0.21;
+console.log(priceWithoutDiscount)
