e0be2494571a51dd3b2193eac6295e12864209c8
Al-Giovinetti <giovinettialessio@gmail.com>
2023-05-12 16:38:13 +0200

    Prezzo biglietto in base all' età

--- js/myscript.js
+++ js/myscript.js
@@ -10,5 +10,30 @@ document.getElementById("user-age").innerHTML += userAge;
 
 //Inizzializzo una variabile per identificare il prezzo non scontato//
 
-let priceWithoutDiscount = kmTravelled * 0.21;
+let priceWithoutDiscount =  kmTravelled * 0.21 ;
 console.log(priceWithoutDiscount)
+
+//Dichiaro una variabile per poi stampare il prezzo scontato//
+
+let priceWhitDiscount;
+
+// Inizzilizzo un a variabile per lo sconto
+
+const specialDiscount = priceWithoutDiscount - priceWhitDiscount
+
+//Se l'età utente è minore di 18 allora
+
+if (userAge < 18){
+    priceWhitDiscount = priceWithoutDiscount -( priceWithoutDiscount / 100 * 20);
+    document.getElementById("final-price").innerHTML = priceWhitDiscount .toFixed(2);
+    document.getElementById("discount").innerHTML += specialDiscount  .toFixed(2)+ ", perchè minorenne";
+}else if(userAge > 65){
+    priceWhitDiscount = priceWithoutDiscount -( priceWithoutDiscount / 100 * 40);
+    document.getElementById("final-price").innerHTML = priceWhitDiscount .toFixed(2);
+    document.getElementById("discount").innerHTML += specialDiscount .toFixed(2) + ", perchè sopra i 65";
+}else{
+    priceWhitDiscount = priceWithoutDiscount 
+    document.getElementById("final-price").innerHTML = priceWhitDiscount .toFixed(2) ;
+    document.getElementById("discount").innerHTML = "prezzo per passeggero standard";
+}
+
